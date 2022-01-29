546f914d5204ad99f75e3a70e87ecebe68cc2701
Taige Wang <taige.wang@aalto.fi>
2022-01-29 09:03:01 +0200

    Change to https server

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const app = require('./app')
-const http = require('http')
+const https = require('https')
+const fs = require('fs')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const server = http.createServer(app)
+const privateKey = fs.readFileSync(__dirname + '/sslcert/server.key', 'utf8')
+const certificate = fs.readFileSync(__dirname + '/sslcert/server.crt', 'utf8')
+
+const credentials = { key: privateKey, cert: certificate }
+const server = https.createServer(credentials, app)
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const backendUrl = 'http://0.0.0.0:3001/api/users'
+const backendUrl = 'https://0.0.0.0:3001/api/users'
 
 const getAllUsers = () => {
   const request = axios.get(backendUrl)
