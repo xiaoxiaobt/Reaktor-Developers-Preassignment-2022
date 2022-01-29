c73917cdc848f58e27519decb67425991b47f2b0
Taige Wang <taige.wang@aalto.fi>
2022-01-29 07:13:12 +0200

    Reconfigure dependencies

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from '@redux-devtools/extension'
-import resultReducer from './reducers/resultReducer'
+// import { createStore, applyMiddleware } from 'redux'
+// // import thunk from 'redux-thunk'
+// import { composeWithDevTools } from '@redux-devtools/extension'
+// import resultReducer from './reducers/resultReducer'
 
-const store = createStore(
-  resultReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk)
-  )
-)
+// const store = createStore(
+//   resultReducer,
+//   composeWithDevTools(
+//     applyMiddleware(thunk)
+//   )
+// )
 
-export default store
+// export default store
